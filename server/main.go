824d011eafa83e4d48ed58650379cf79c9de00a8
Graham Greving <ggreving@ucsc.edu>
2015-02-13 20:04:27 -0800

    Support for named clients

--- server/main.go
+++ server/main.go
@@ -11,14 +11,17 @@ import (
 
 // Some sort of a client datatype
 
+// This is the message broadcaster
+// All clients write to this goroutines channel
+// To broadcast messages to the server
 func server(c <-chan string) {
     for msg := range c {
         fmt.Println(msg)
     }
 }
 
-func handleConnection(conn net.Conn, s_chan chan string) {
-    var b =  []byte("hey welcome to codechat\n")
+func handleConnection(conn net.Conn, serv *Server, s_chan chan string) {
+    b := []byte("hey welcome to codechat\n")
     fmt.Println("new connection!")
     // Read first message from client
     // Parse message for commands
@@ -31,19 +34,30 @@ func handleConnection(conn net.Conn, s_chan chan string) {
         return
     }
     buf := make ([]byte, 4096)
- 
+    n, err:= conn.Read(buf)
+    if err != nil || n == 0 {
+        fmt.Println(err)
+        conn.Close()
+        return
+    }
+    writeChan := make(chan string)
+    client := Client{string(buf[0:n-2]),writeChan}
+    serv.clients[conn] = client
+    getClients(serv)
     for {
         n, err:= conn.Read(buf)
         if err != nil || n == 0 {
             fmt.Println(err)
             conn.Close()
             break
-        }    
+        }
         //n, err = conn.Write(buf[0:n])
         msg := string(buf[0:n-2])
         if msg == "exit" {
+            delete(serv.clients, conn)
             conn.Close()
             msg = "Client Left"
+            getClients(serv)
             s_chan <- msg
             return
         }
@@ -56,10 +70,27 @@ func handleConnection(conn net.Conn, s_chan chan string) {
     }
 }
 
+type Server struct {
+    clients map[net.Conn] Client
+}
+
+type Client struct {
+    name string
+    channel chan string
+}
+
+func getClients(serv *Server) {
+    for _,value := range serv.clients {
+        fmt.Println(value.name)
+    }
+}
+
 func main() {
     fmt.Println("CodeChat Server Starting")
     s_chan := make(chan string)
     go server(s_chan)
+    serv := new(Server)
+    serv.clients = make(map[net.Conn] Client)
     ln,err := net.Listen("tcp",":8080")
 
     if err != nil {
@@ -72,7 +103,6 @@ func main() {
             fmt.Println("Error! Bad accept.")
             break
         }
-    
-        go handleConnection(conn, s_chan)
+        go handleConnection(conn, serv, s_chan)
     }
 }
