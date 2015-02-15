cca60c66641c71634785ccf4050a114cfc4d1619
Graham Greving <ggreving@ucsc.edu>
2015-02-15 12:02:23 -0800

    Added a simple test client for the server

---
+++ client/main.go
@@ -0,0 +1,26 @@
+// Simple client for CodeChat server
+
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	c,err := net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer c.Close()
+	b := []byte(`{"cmd":"connect","username":"gramasaurous"}`)
+	log.Println(b)
+	n,err := c.Write(b)
+	if err != nil || n == 0 {
+		log.Println(err)
+		return
+	}
+	// keep the connection alive
+	for {}
+}
