056ee8bde10de13191180669f5e5a546e08a97de
Graham Greving <ggreving@ucsc.edu>
2015-03-06 11:06:47 -0800

    slight code refactor

--- gtkclient/gtkclient.go
+++ gtkclient/gtkclient.go
@@ -12,32 +12,32 @@ import (
 	// "strings"
 )
 
-func main() {
-	//var menuitem *gtk.MenuItem
-	gtk.Init(nil)
-	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
-	window.SetPosition(gtk.WIN_POS_CENTER)
-	window.SetTitle("CodeChat")
-	window.SetIconName("gtk-dialog-info")
-	window.Connect("destroy", func(ctx *glib.CallbackContext) {
-		println("got destroy!", ctx.Data().(string))
-		gtk.MainQuit()
-	}, "CodeChat")
+type Layout struct {
+	// frames + panes
+	mainFrame  gtk.IWidget
+	leftFrame  gtk.IWidget
+	rightFrame gtk.IWidget
 
-	/*
-								main
-		   +----------------------------------------+
-		   |		left		|		right		|
-		   |					|					|
-		   |					|					|
-		   |					|					|
-		   |					|					|
-		   |					+-------------------+
-		   |					|				|btn|
-		   +--------------------+-------------------+
+	// objects
+	editor      gtk.IWidget
+	inputEntry  gtk.IWidget
+	inputButton gtk.IWidget
+}
 
-	*/
+/*
+							main
+	+----------------------------------------+
+	|		left		|		right		|
+	|					|					|
+	|					|					|
+	|					|					|
+	|					|					|
+	|					+-------------------+
+	|					|				|btn|
+	+--------------------+-------------------+
 
+*/
+func layoutInit() Layout {
 	// layout frames
 	mainFrame := gtk.NewFrame("")
 	mainBox := gtk.NewHBox(true, 1)
@@ -95,7 +95,24 @@ func main() {
 	chatFrame.SetSizeRequest(500, 550)
 	inputFrame.SetSizeRequest(500, 50)
 
-	window.Add(mainFrame)
+	return Layout{mainFrame, leftFrame, rightPane, editor, inputEntry, inputButton}
+}
+
+func main() {
+	//var menuitem *gtk.MenuItem
+	gtk.Init(nil)
+	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	window.SetPosition(gtk.WIN_POS_CENTER)
+	window.SetTitle("CodeChat")
+	window.SetIconName("gtk-dialog-info")
+	window.Connect("destroy", func(ctx *glib.CallbackContext) {
+		println("got destroy!", ctx.Data().(string))
+		gtk.MainQuit()
+	}, "CodeChat")
+
+	layout := layoutInit()
+
+	window.Add(layout.mainFrame)
 	window.SetSizeRequest(1000, 600)
 	window.ShowAll()
 	gtk.Main()
