76f503f535b9b96c6c150327000bfc388993c8ec
Graham Greving <ggreving@ucsc.edu>
2015-03-08 14:26:13 -0700

    refactoring client code into a library
    this package is now importable.

--- client/client.go
+++ client/client.go
@@ -1,110 +1,112 @@
 // Simple client for CodeChat server
 
-package main
+package client
 
 import (
-	// gc "code.google.com/p/goncurses"
 	"encoding/json"
-	"flag"
-	"fmt"
-	"log"
+	//"flag"
+	//"fmt"
+	"errors"
+	//"log"
 	"net"
-	"os"
-	"strings"
+	//"os"
+	//"strings"
 )
 
-type Message struct {
+type WriteMessage struct {
 	Cmd string `json:"cmd"`
 	Msg string `json:"msg"`
 }
 
-type Connect struct {
+type ReadMessage struct {
+	Cmd string
+	From string
+	Payload string
+}
+
+type ConnectMsg struct {
 	Cmd      string `json:"cmd"`
 	Username string `json:"username"`
 }
 
-func read(conn net.Conn) {
-	// b := make([]byte, 4096)
-	d := json.NewDecoder(conn)
-	for {
-		var v map[string]interface{}
-		err := d.Decode(&v)
-		if err != nil {
-			log.Println("error, bad json")
-			// should exit here : signifies a dead server
-			return
-		}
-		// Catch a response from the server
-		if s, ok := v["success"]; ok {
-			if s.(bool) {
-				log.Println("success")
-			} else {
-				log.Println("read: command failed")
-				log.Println("returned: ", v["status-message"])
-			}
-			// Catch general messages from the server
-		} else if c, ok := v["cmd"]; ok {
-			switch c {
-			case "message":
-				log.Println("got message")
-			case "client-exit":
-				log.Println("client exited")
-			case "client-connect":
-				log.Println("client entered")
-			default:
-				log.Println("no cmd parsed. got: ", v)
-			}
-		} else {
-			log.Println("json parsing failed, got: ", v)
-		}
-	}
+type Client struct {
+	Username string
+	Conn	net.Conn
+	Jreader *json.Decoder
+	Write	bool
 }
 
-func main() {
-	flag.Parse()
-	args := flag.Args()
-	if len(args) < 1 {
-		fmt.Println("need a name")
-		return
-	}
-	name := args[0]
-	log.Println(name)
 
-	c, err := net.Dial("tcp", "127.0.0.1:8080")
+// Read reads in a message from the server, catches any errors and
+// repackages it as a nice exported struct for easy handling server
+// commands
+func (client *Client) Read() (ReadMessage, error){
+	var v map[string]interface{}
+	var retMsg ReadMessage
+	err := client.Jreader.Decode(&v)
 	if err != nil {
-		log.Println(err)
-		return
+		// should exit here : signifies a dead server
+		return retMsg, err
 	}
-	defer c.Close()
-
-	go read(c)
-
-	user := Connect{"connect", name}
-	b, err := json.Marshal(user)
-
-	n, err := c.Write(b)
-	if err != nil || n == 0 {
-		log.Println(err)
-		return
-	}
-
-	// keep the connection alive
-	for {
-		read := make([]byte, 4096)
-		n, err := os.Stdin.Read(read)
-		if err != nil || n == 0 {
-			log.Println(err)
-			return
+	// Catch a response from the server
+	if s, ok := v["success"]; ok {
+		if s.(bool) {
+			retMsg.Cmd = "success"
+			retMsg.From = "server"
+		} else {
+			err = errors.New("Read: previous command failed")
 		}
-		readStr := strings.TrimSpace(string(read))
-		m := Message{"msg", readStr}
-		//fmt.Println(m)
-		b, e := json.Marshal(m)
-		if e != nil {
-			log.Println("somethin happened...")
-			continue
+		// Catch general messages from the server
+	} else if c, ok := v["cmd"]; ok {
+		switch c {
+		case "message":
+			retMsg.Cmd = c.(string)
+		case "client-exit":
+			retMsg.Cmd = c.(string)
+		case "client-connect":
+			retMsg.Cmd = c.(string)
+		case "update-file":
+			retMsg.Cmd = c.(string)
+		default:
+			// catches when a client EOFs
+			err = errors.New("Read: no cmd parsed")
 		}
-		c.Write(b)
-		//fmt.Println(b)
+		if f, ok := v["from"]; ok {
+			retMsg.From = f.(string)
+		} else {
+			err = errors.New("Read: no from given")
+		}
+		if p, ok := v["payload"]; ok {
+			// acceptable error
+			retMsg.Payload = p.(string)
+		} else {
+			// acceptable error
+			err = errors.New("Read: no payload given")
+		}
+	} else {
+		err = errors.New("Read: json parsing failed")
 	}
+	return retMsg, err
 }
+
+func Connect(username string) (*Client, error) {
+	var c = new(Client)
+	var err error
+	c.Username = username
+	// make the connection to the server
+	c.Conn, err = net.Dial("tcp", "127.0.0.1:8080")
+	if err != nil {
+		return nil, err
+	}
+	c.Jreader = json.NewDecoder(c.Conn)
+	user := ConnectMsg{"connect", c.Username}
+	
+	b, err := json.Marshal(user)
+	n, err := c.Conn.Write(b)
+	if err != nil || n == 0 {
+		return nil, err
+	}
+	c.Write = false
+	return c, err
+}
+
