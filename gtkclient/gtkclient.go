abf9f480c3f60d30fd31d382e623e6c2165472f4
Graham Greving <ggreving@ucsc.edu>
2015-03-08 22:27:25 -0700

    back to where we started - but the code is much nicer
    i took the time to refactor the bulk of the GTK code into a
    simple library. now the gtkclient main is much nicer.

--- gtkclient/gtkclient.go
+++ gtkclient/gtkclient.go
@@ -2,96 +2,13 @@ package main
 
 import (
 	codechat "CodeChat/client"
-	"github.com/mattn/go-gtk/glib"
+	"CodeChat/layout"
 	"github.com/mattn/go-gtk/gtk"
-	"github.com/mattn/go-gtk/gtksourceview"
 	"log"
 )
 
-type Layout struct {
-	// frames + panes
-	mainFrame  	*gtk.Frame
-	left  		*gtk.Frame
-	right 		*gtk.VPaned
-
-	leftEvent	*gtk.EventBox
-	rightEvent  *gtk.EventBox
-
-	// objects
-	editor       *gtksourceview.SourceView
-	editorBuf    *gtksourceview.SourceBuffer
-	inputEntry   *gtk.Entry
-	inputButton  *gtk.Button
-	chatMessages *gtk.TextView
-}
-
-/*
-							main
-	+----------------------------------------+
-	|		left		|		right		|
-	|					|					|
-	|					|					|
-	|					|					|
-	|					|					|
-	|					+-------------------+
-	|					|				|btn|
-	+--------------------+-------------------+
-
-*/
-func layoutInit() Layout {
-	// layout frames
-	mainFrame := gtk.NewFrame("")
-	mainBox := gtk.NewHBox(true, 1)
-	leftFrame := gtk.NewFrame("editor")
-	leftBox := gtk.NewScrolledWindow(nil, nil)
-	leftBox.SetPolicy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-	leftBox.SetShadowType(gtk.SHADOW_IN)
-	// need to figure out this sourceview thing
-	editorBuf := gtksourceview.NewSourceBuffer()
-	editor := gtksourceview.NewSourceViewWithBuffer(editorBuf)
-	leftBox.Add(editor)
-	leftFrame.Add(leftBox)
-	rightPane := gtk.NewVPaned()
-	chatFrame := gtk.NewFrame("chat")
-	chatBox := gtk.NewScrolledWindow(nil, nil)
-	chatBox.SetPolicy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-	chatBox.SetShadowType(gtk.SHADOW_IN)
-	chatMessages := gtk.NewTextView()
-	chatMessages.SetEditable(false)
-	chatMessages.SetCursorVisible(false)
-	chatBox.Add(chatMessages)
-	chatFrame.Add(chatBox)
-	inputFrame := gtk.NewFrame("")
-	inputBox := gtk.NewHBox(false, 1)
-	inputEntry := gtk.NewEntry()
-	inputButton := gtk.NewButtonWithLabel("send")
-	inputBox.Add(inputEntry)
-	inputBox.Add(inputButton)
-	inputFrame.Add(inputBox)
-	rightPane.Pack1(chatFrame, false, false)
-	rightPane.Pack2(inputFrame, false, false)
-	mainBox.Add(leftFrame)
-	mainBox.Add(rightPane)
-	mainFrame.Add(mainBox)
-	inputEntry.SetSizeRequest(450, -1)
-	chatFrame.SetSizeRequest(500, 550)
-	inputFrame.SetSizeRequest(500, 50)
-
-	return Layout{mainFrame, leftFrame, rightPane, leftEvent, rightEvent, editor, editorBuf, inputEntry, inputButton, chatMessages}
-}
-
-type Message struct {
-	Cmd string `json:"cmd"`
-	Msg string `json:"msg"`
-}
-
-type Connect struct {
-	Cmd      string `json:"cmd"`
-	Username string `json:"username"`
-}
-
-func doRead(client *codechat.Client, lyt *Layout) {
-	buffer := lyt.chatMessages.GetBuffer()
+func doRead(client *codechat.Client, lyt *layout.Layout) {
+	buffer := lyt.ChatBuffer
 	var end gtk.TextIter
 	for {
 		read, err := client.Read()
@@ -119,61 +36,48 @@ func doRead(client *codechat.Client, lyt *Layout) {
 	}
 }
 
-func main() {
-	//var menuitem *gtk.MenuItem
+func messageAction(client *codechat.Client, lyt *layout.Layout) {
+	msg := lyt.MessageBuffer.GetText()
+	println("button clicked: ", msg)
+	var end gtk.TextIter
+	buffer := lyt.ChatBuffer
+	buffer.GetEndIter(&end)
+	buffer.Insert(&end, "you: "+msg+"\n")
+	if s:=len(msg); s != 0 {
+		lyt.MessageBuffer.DeleteText(0, s)
+	}
+	// write msg here
+	client.Write("msg", msg)
+}
 
+func main() {
 	var name string
 	var err error
 	var client *codechat.Client
 
-	gtk.Init(nil)
-	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
-	window.SetPosition(gtk.WIN_POS_CENTER)
-	window.SetTitle("CodeChat")
-	window.SetIconName("gtk-dialog-info")
-	window.Connect("destroy", func(ctx *glib.CallbackContext) {
-		println("got destroy!", ctx.Data().(string))
-		gtk.MainQuit()
-	}, "CodeChat")
+	lyt := new(layout.Layout)
 
-	layout := layoutInit()
+	lyt.Init()
 
 	// Send a message to the server when the input button is clicked
-	layout.inputButton.Clicked(func() {
-		// send mesage here
-		msg := layout.inputEntry.GetText()
-		println("button clicked: ", msg)
-		var end gtk.TextIter
-		buffer := layout.chatMessages.GetBuffer()
-		buffer.GetEndIter(&end)
-		buffer.Insert(&end, "you: "+msg+"\n")
-		layout.inputEntry.SetText("")
-		// write msg here
-		client.Write("msg", msg)
+	lyt.SendBtn.Clicked(func () {
+		messageAction(client, lyt)
 	})
 	// Send a message to the server when the user hits enter in the text
 	// input
-	layout.inputEntry.Connect("activate", func() {
-		msg := layout.inputEntry.GetText()
-		println("enter pressed: ", msg)
-		var end gtk.TextIter
-		buffer := layout.chatMessages.GetBuffer()
-		buffer.GetEndIter(&end)
-		buffer.Insert(&end, "you: "+msg+"\n")
-		layout.inputEntry.SetText("")
-		// write msg here
-		client.Write("msg", msg)
+	lyt.ChatEntry.Connect("activate", func () {
+		messageAction(client, lyt)
 	})
 
-	// User has entered some text in the editor window
-	// send update-file 
-	layout.editorBuf.Connect("end-user-action", func() {
-		var start, end gtk.TextIter
-		layout.editorBuf.GetStartIter(&start)
-		layout.editorBuf.GetEndIter(&end)
-		log.Println("New file:")
-		log.Println(layout.editorBuf.GetText(&start, &end, true))
-	})
+	//// User has entered some text in the editor window
+	//// send update-file 
+	//layout.editorBuf.Connect("end-user-action", func() {
+		//var start, end gtk.TextIter
+		//layout.editorBuf.GetStartIter(&start)
+		//layout.editorBuf.GetEndIter(&end)
+		//log.Println("New file:")
+		//log.Println(layout.editorBuf.GetText(&start, &end, true))
+	//})
 
 
 	// When focus enters the right side (editor):
@@ -186,64 +90,22 @@ func main() {
 	//		- if true -> set left editable and let the edits flow
 	//		- if false -> wait (..?)
 
-	layout.left.Connect("move-focus", func() {
-		log.Println("left side move focus.")
-	})
-	
-	layout.right.Connect("move-focus", func() {
-		log.Println("right side move focus.")
-	})
 
-	layout.left.Connect("grab-focus", func() {
-		log.Println("left side got focus.")
-	})
-	
-	layout.right.Connect("grab-focus", func() {
-		log.Println("right side got focus.")
-	})
-
-	// Show a dialog to get the username on startup
-	messagedialog := gtk.NewDialog()
-	connectBox := messagedialog.GetVBox()
-	label := gtk.NewLabel("username")
-	username := gtk.NewEntry()
-	username.Connect("activate", func() {
-		name = username.GetText()
-		messagedialog.Destroy()
-	})
-	connectBox.Add(label)
-	connectBox.Add(username)
-	messagedialog.AddButton("connect", gtk.RESPONSE_OK)
-	messagedialog.Response(func() {
-		name = username.GetText()
-	})
-	label.Show()
-	username.Show()
-	messagedialog.Run()
-	messagedialog.Destroy()
-	// End Dialog
-	
-	// Setup the window
-	window.Add(layout.mainFrame)
-	window.SetSizeRequest(1000, 600)
-	window.ShowAll()
 
 	// Connect the client
+	name = layout.PromptUsername()
 	client, err = codechat.Connect(name)
-	
 	if err != nil {
 		log.Println("could not connect")
 		return
 	} else {
 		var end gtk.TextIter
-		buffer := layout.chatMessages.GetBuffer()
-		buffer.GetEndIter(&end)
-		buffer.Insert(&end, "Successfully connected as " + client.Username + "\n")
+		lyt.ChatBuffer.GetEndIter(&end)
+		lyt.ChatBuffer.Insert(&end, "Successfully connected as " + client.Username + "\n")
 	}
 
 	defer client.Close("F THIS")
 
-	go doRead(client, &layout)
-
+	go doRead(client, lyt)
 	gtk.Main()
 }
