6a87e54be07e1850358433a1cd2bfbd8c38a86c2
Graham Greving <ggreving@ucsc.edu>
2015-03-11 16:22:40 -0700

    layout is a class now

--- pygtk.py
+++ pygtk.py
@@ -13,88 +13,115 @@ def win_quit():
 	print("peace out!")
 	gtk.MainQuit()
 
-def layout():
-	win = gtk.Window()
-	win.set_position(gtk.WIN_POS_CENTER)
-	win.set_title("CodeChat")
-	win.set_icon_name("gtk-dialog-info")
-	win.connect("destroy", win_quit)
+class Layout:
 
-	leftEvent = gtk.EventBox()
-	rightEvent = gtk.EventBox()
-	#// main horizontal container, equal 1px spacing
-	mainBox = gtk.HBox(True, 1)
+	def __init__(self):
+		win = gtk.Window()
+		win.set_position(gtk.WIN_POS_CENTER)
+		win.set_title("CodeChat")
+		win.set_icon_name("gtk-dialog-info")
+		win.connect("destroy", win_quit)
 
-	leftFrame = gtk.Frame("editor")
-	rightFrame = gtk.Frame("chat")
+		leftEvent = gtk.EventBox()
+		rightEvent = gtk.EventBox()
+		#// main horizontal container, equal 1px spacing
+		mainBox = gtk.HBox(True, 1)
 
-	#// left and right containers, spacing not equal
-	leftVBox = gtk.VBox(False, 1)
-	rightVBox = gtk.VBox(False, 1)
+		leftFrame = gtk.Frame("editor")
+		rightFrame = gtk.Frame("chat")
 
-	#// build up the left side
-	editWindow = gtk.ScrolledWindow(None,None)
-	editWindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-	editWindow.set_shadow_type(gtk.SHADOW_IN)
+		#// left and right containers, spacing not equal
+		leftVBox = gtk.VBox(False, 1)
+		rightVBox = gtk.VBox(False, 1)
 
-	EditBuffer = gtksourceview2.Buffer()
-	EditLangMgr = gtksourceview2.LanguageManager()
-	EditBuffer.set_highlight_syntax(True)
-	EditBuffer.set_highlight_matching_brackets(True)
-	lang = EditLangMgr.get_language("python")
-	EditBuffer.set_language(lang)
+		#// build up the left side
+		editWindow = gtk.ScrolledWindow(None,None)
+		editWindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+		editWindow.set_shadow_type(gtk.SHADOW_IN)
 
-	EditView = gtksourceview2.View(EditBuffer)
-	EditView.set_highlight_current_line(True)
-	EditView.set_show_line_numbers(True)
-	EditView.modify_font(pango.FontDescription("Monospace 8"))
-	EditStatusBar = gtk.Statusbar()
-	editWindow.add(EditView)
-	
-	leftVBox.pack_start(editWindow, True, True, 1)
-	leftVBox.pack_end(EditStatusBar, False, False, 2)
-	
-	leftFrame.add(leftVBox)
+		self.EditBuffer = gtksourceview2.Buffer()
+		self.EditLangMgr = gtksourceview2.LanguageManager()
+		self.EditBuffer.set_highlight_syntax(True)
+		self.EditBuffer.set_highlight_matching_brackets(True)
+		lang = self.EditLangMgr.get_language("python")
+		self.EditBuffer.set_language(lang)
 
-	#// build up the right side
-	chatWindow = gtk.ScrolledWindow(None, None)
-	chatWindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
-	chatWindow.set_shadow_type(gtk.SHADOW_IN)
-	
-	chatTagTable = gtk.TextTagTable()
-	ChatBuffer = gtk.TextBuffer(chatTagTable)
-	ChatView = gtk.TextView(ChatBuffer)
-	ChatView.set_editable(False)
-	ChatView.set_cursor_visible(False)
-	chatWindow.add(ChatView)
-	chatHBox = gtk.HBox(False, 0)
-	MessageBuffer = gtk.EntryBuffer("", -1)
-	ChatEntry = gtk.Entry()
-	ChatEntry.set_buffer(MessageBuffer)
-	SendBtn = gtk.Button("send")
+		self.EditView = gtksourceview2.View(self.EditBuffer)
+		self.EditView.set_highlight_current_line(True)
+		self.EditView.set_show_line_numbers(True)
+		self.EditView.modify_font(pango.FontDescription("Monospace 8"))
+		self.EditStatusBar = gtk.Statusbar()
+		editWindow.add(self.EditView)
+		
+		leftVBox.pack_start(editWindow, True, True, 1)
+		leftVBox.pack_end(self.EditStatusBar, False, False, 2)
+		
+		leftFrame.add(leftVBox)
 
-	chatHBox.pack_start(ChatEntry, True, True, 0)
-	chatHBox.pack_end(SendBtn, False, False, 0)
+		#// build up the right side
+		chatWindow = gtk.ScrolledWindow(None, None)
+		chatWindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+		chatWindow.set_shadow_type(gtk.SHADOW_IN)
+		
+		chatTagTable = gtk.TextTagTable()
+		self.ChatBuffer = gtk.TextBuffer(chatTagTable)
+		self.ChatView = gtk.TextView(self.ChatBuffer)
+		self.ChatView.set_editable(False)
+		self.ChatView.set_cursor_visible(False)
+		chatWindow.add(self.ChatView)
+		chatHBox = gtk.HBox(False, 0)
+		self.MessageBuffer = gtk.EntryBuffer("", -1)
+		self.ChatEntry = gtk.Entry()
+		self.ChatEntry.set_buffer(self.MessageBuffer)
+		self.SendBtn = gtk.Button("send")
 
-	rightVBox.pack_start(chatWindow, True, True, 1)
-	rightVBox.pack_start(chatHBox, False, False, 0)
-	
-	rightFrame.add(rightVBox)
-	
-	leftEvent.add(leftFrame)
-	rightEvent.add(rightFrame)
-	
-	#// set up interface + layout
-	mainBox.pack_start(leftEvent, True, True, 0)
-	mainBox.pack_start(rightEvent, True, True, 0)
-	
-	#// Show the window
-	win.add(mainBox)
-	win.set_size_request(800, 400)
-	win.show_all()
+		chatHBox.pack_start(self.ChatEntry, True, True, 0)
+		chatHBox.pack_end(self.SendBtn, False, False, 0)
+
+		rightVBox.pack_start(chatWindow, True, True, 1)
+		rightVBox.pack_start(chatHBox, False, False, 0)
+		
+		rightFrame.add(rightVBox)
+		
+		leftEvent.add(leftFrame)
+		rightEvent.add(rightFrame)
+		
+		#// set up interface + layout
+		mainBox.pack_start(leftEvent, True, True, 0)
+		mainBox.pack_start(rightEvent, True, True, 0)
+		
+		#// Show the window
+		win.add(mainBox)
+		win.set_size_request(800, 400)
+		win.show_all()
+
+	def messageAction():
+		print("message Action:")
 
 def main():
-	layout()
+	lyt = Layout()
+
+	lyt.SendBtn.connect("clicked", lyt.messageAction)
+
+	#lyt.ChatEntry.Connect("activate", func () {
+		#messageAction(client, lyt)
+	#})
+
+	#// User has entered some text in the editor window
+	#// send update-file 
+	#lyt.EditBuffer.Connect("end-user-action", func() {
+		#WriteLock.Lock()
+		#var start, end gtk.TextIter
+		#ctx := lyt.EditStatusBar.GetContextId("CodeChat")
+		#lyt.EditStatusBar.Pop(ctx)
+		#lyt.EditStatusBar.Push(ctx, "last edited by you")
+		#lyt.EditBuffer.GetStartIter(&start)
+		#lyt.EditBuffer.GetEndIter(&end)
+		#file := lyt.EditBuffer.GetText(&start, &end, true)
+		#client.Write("update-file",file)
+		#WriteLock.Unlock()
+	#})
+
 	# need to:
 	# [] connect to the server
 	# [] connect event handlers
